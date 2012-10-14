ab2eda7b9639eb53addfe926d9fc5cb23b61f938
Vincent Dorie <vjd4@nyu.edu>
2012-10-14 00:00:00 +0000

    version 1.0-0

--- R/commonScale_defaults.R
+++
@@ -1,8 +0,0 @@
-loadCommonScaleDefaults <- function(env) {
-  env$defaultCommonScalePointPosteriorScale <- SD_SCALE_NAME;
-  env$defaultCommonScalePointPriorValue <- 1.0;
-
-  env$defaultCommonScaleInverseGammaShape <- 0;
-  env$defaultCommonScaleInverseGammaScale <- 0;
-  env$defaultCommonScaleInverseGammaPosteriorScale <- VAR_SCALE_NAME;
-}
--- src/string.h
+++
@@ -1,8 +0,0 @@
-#ifndef BLME_STRING_H
-#define BLME_STRING_H
-
-#include <R.h>
-
-SEXP bmer_findCommaInList(SEXP input);
-
-#endif // BLME_STRING_H
